ee09724bfe924d500cd132c687574eec2d95a549
Paulo Pinto <ppinto@gbm.net>
2021-02-25 19:15:29 -0500

    Include import

--- src/main/java/com/gbm/samples/greeterjava/GreetingController.java
+++ src/main/java/com/gbm/samples/greeterjava/GreetingController.java
@@ -4,7 +4,7 @@ package com.gbm.samples.greeterjava;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
